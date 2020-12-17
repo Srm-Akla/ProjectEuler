21104d3619af92d931028257a15bdae33e6027ef
Srm-Akla <akellamahateja@gmail.com>
2020-12-17 21:57:03 +0100

    Adding comments in the beginning

---
+++ 9.cpp
@@ -0,0 +1,13 @@
+/*
+ * Author: Sriram Akella
+ * Url: https://projecteuler.net/problem=9
+ * Github: 
+ *
+ * Problem:
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *		    a² + b² = c²
+ * For example, 3² + 4² = 5² => 9 + 16 = 25
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+
